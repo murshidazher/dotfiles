c4d9a247a169dac76133410be3c7d8584967bde8
murshidazher <murshidazher@yahoo.com>
2021-09-09 01:42:53 +0530

    :recycle: refactor: change the install chrome script

--- chrome/install-chrome.sh
+++ chrome/install-chrome.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-line="$1"
-extArr=($(echo "$line" | tr '/' '\n'))
-echo "${extArr[1]}"
+# Set default extension URL base
+URL_CHROME="https://clients2.google.com/service/update2/crx"
 
-preferences_dir_path="$HOME/Library/Application Support/Google/Chrome/External Extensions"
-pref_file_path="$preferences_dir_path/${extArr[1]}.json"
-upd_url="https://clients2.google.com/service/update2/crx"
-mkdir -p "$preferences_dir_path"
-echo "{" >"$pref_file_path"
-echo "  \"external_update_url\": \"$upd_url\"" >>"$pref_file_path"
-echo "}" >>"$pref_file_path"
-echo Added \""$pref_file_path"\" ["${extArr[0]}"]
+# Set global extensions installation path
+PATH_CHROME_EXT="$HOME/Library/Application Support/Google/Chrome/External Extensions"
+
+# Break the extension input to determine the slug and extension id
+EXT_ARRAY=($(echo "$1" | tr '/' '\n'))
+EXT_SLUG="${EXT_ARRAY[0]}"
+EXT_ID="${EXT_ARRAY[1]}"
+
+# Setup extension path
+[ ! -d "$PATH_CHROME_EXT" ] && mkdir -p "$PATH_CHROME_EXT"
+
+# Install extension
+UID_ADDON_PATH="$PATH_CHROME_EXT/${EXT_ID}.json"
+
+# Create extension file
+echo "{" >"$UID_ADDON_PATH"
+echo "  \"external_update_url\": \"$URL_CHROME\"" >>"$UID_ADDON_PATH"
+echo "}" >>"$UID_ADDON_PATH"
+
+# End message
+echo Added \""$UID_ADDON_PATH"\" ["${EXT_SLUG}"]
--- chrome/test.sh
+++ chrome/test.sh
@@ -5,5 +5,12 @@ if [ -z ${defaultdotfilesdir+x} ]; then
   defaultdotfilesdir="$HOME/dotfiles"
 fi
 
+install_y() {
+  echo "$1"
+  echo "end"
+}
+
 # Install chrome extensions
-cat $defaultdotfilesdir/chrome/test-extensions.list | xargs -I {} ./install-chrome.sh {}
+# cat $defaultdotfilesdir/chrome/test-extensions.list | xargs -I {} ./install-chrome.sh {}
+# Install chrome extensions
+cat $defaultdotfilesdir/chrome/test-extensions.list | xargs -I {} ./install_y {}
