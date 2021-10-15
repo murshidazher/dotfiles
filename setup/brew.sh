7732f1960c6dde766153050ec916b4250ff6b64b
murshidazher <murshidazher@yahoo.com>
2021-10-15 11:53:22 +0530

    :sparkles: feat: add simplenote

--- setup/brew.sh
+++ setup/brew.sh
@@ -381,6 +381,7 @@ if $brewinstall; then
     mas install 1474276998 # HP Smart for Desktop
     mas install 490461369  # Bandwidth+
     mas install 1056643111 # Clocker
+    mas install 692867256  # Simplenote
 
     ## Password management
     # mas install 1191757556 # Obsidian Authenticator App
